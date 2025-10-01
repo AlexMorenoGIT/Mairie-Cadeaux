9f47dd049a4849c09c06add459cb7b0aae8b3cb2
alexmoreno <alex.m@wikicampers.com>
2025-10-01 17:08:08 +0200

    Adding vite project

---
+++ mairie_cadeaux/frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello CRUD avec Tailwind v4 🚀
+      </h1>
+    </div>
+  );
+}
